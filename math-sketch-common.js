628a4f9c56ec1818cddeac78272f4987a30ee18c
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-11-18 14:44:16 +0000

    Mouse drag and release states

--- math-sketch-common.js
+++ math-sketch-common.js
@@ -39,18 +39,79 @@
     let interaction_cbs =
     {
         mouse_pressed : [],
+        mouse_dragged : [],
+        mouse_released : [],
     };
 
-    MS.add_interaction_cb = (cb) => interaction_cbs.push(x);
+    MS.add_interaction_cb = function ( {mouse_pressed, mouse_dragged}={})
+    {
+        if ( mouse_pressed )
+            interaction_cbs.mouse_pressed.push(mouse_pressed);
+
+        if ( mouse_released )
+            interaction_cbs.mouse_released.push(mouse_released);
+    }
+
+    const InteractionStates = {
+        Idle : "Idle",
+        Dragging : "Dragging",
+    }
+
+    let interaction_state = InteractionStates.Idle;
+    
+    let drag_cb = null;
 
     MS.setup_interaction_hooks = function ()
     {
         mousePressed_ = mousePressed;
-        mousePressed = () => 
+        mousePressed = ( e ) => 
         {
-            mousePressed_();
-            interaction_cbs.mousePressed_.forEach( f => f() );
+            if ( interaction_state != InteractionStates.Idle )
+                return;
+
+            mousePressed_( e );
+            interaction_cbs.mouse_pressed.forEach( f => f( e ) );
         };
+
+        mouseDragged_ = mouseDragged;
+        mouseDragged = ( e ) => 
+        {
+            mouseDragged_( e );
+
+            if ( interaction_state != InteractionStates.Dragging )
+                return;
+
+            drag_cb();
+        };
+
+        mouseReleased_ = mouseReleased;
+        mouseReleased = ( e ) => 
+        {
+            if ( interaction_state == InteractionStates.Dragging )
+                MS.end_drag();
+
+            mouseReleased_( e );
+            interaction_cbs.mouse_released.forEach( f => f( e ) );
+        };
+
+    };
+
+    MS.start_drag = function ( cb )
+    {
+        if ( interaction_state != InteractionStates.Idle )
+            return;
+
+        interaction_state = InteractionStates.Dragging;
+        drag_cb = cb;
+    };
+
+    MS.end_drag = function ()
+    {
+        if ( interaction_state != InteractionStates.Dragging )
+            return;
+
+        drag_cb = null;
+        interaction_state = InteractionStates.Idle;
     };
 
 
